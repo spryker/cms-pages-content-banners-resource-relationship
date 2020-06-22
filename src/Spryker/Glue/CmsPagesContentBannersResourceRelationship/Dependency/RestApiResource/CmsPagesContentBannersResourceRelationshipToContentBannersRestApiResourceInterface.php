421a5f56a3c948b5b4fce6c3ec89faa06a92ed1c
Kotiuk <andrii.kotiuk@spryker.com>
2020-06-22 11:00:33 +0300

    mp-2855 created content banners - cms-page connector module

---
+++ src/Spryker/Glue/CmsPagesContentBannersResourceRelationship/Dependency/RestApiResource/CmsPagesContentBannersResourceRelationshipToContentBannersRestApiResourceInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Glue\CmsPagesContentBannersResourceRelationship\Dependency\RestApiResource;
+
+use Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface;
+
+interface CmsPagesContentBannersResourceRelationshipToContentBannersRestApiResourceInterface
+{
+    /**
+     * @phpstan-param array<string, string> $contentBannerKeys
+     *
+     * @phpstan-return array<string, \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceInterface>
+     *
+     * @param string[] $contentBannerKeys
+     * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
+     *
+     * @return \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceInterface[]
+     */
+    public function getContentBannersByKeys(array $contentBannerKeys, RestRequestInterface $restRequest): array;
+}
